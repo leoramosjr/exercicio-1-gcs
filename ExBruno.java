08737e8e82f790b74511bbaff8db3bf04261ba14
BRUNO BRITO <souza.bruno96@edu.pucrs.br>
2024-03-15 18:57:32 -0300

    fiz uma parte do exD

--- ExBruno.java
+++ ExBruno.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
 public class ExBruno {
-    public static void main(String[] args) {
-        System.out.println("Hi");
+   /* 
+    * 
+      d. Método que retorna uma lista de elementos repetidos de l
+        		 ArrayList<Integer> listRepeat(ArrayList< Integer > l)
+    */
+    public static ArrayList<Integer> exD (ArrayList<Integer> l){
+        ArrayList<Integer> lista = new ArrayList<Integer>();
+        
+        for(int i = 0; i < l.size(); i++) {
+            Integer elemento = l.get(i);
+            for(int j = 0; j < l.size(); j++){
+                Integer elemento2 = l.get(j);
+                if((i != j) && (elemento == elemento2) && (lista.get(i) != null)){
+                    lista.add(l.get(i));
+                    
+                }
+            }
+        }
+        return lista;
     }
 }
--- Main.java
+++ Main.java
@@ -1,6 +1,22 @@
+import java.util.ArrayList;
+ 
 public class Main {
     public static void main(String[] args) {
         App app = new App();
         app.executar();
+        ArrayList<Integer> sampleLista = new ArrayList<Integer>();
+        sampleLista.add(34);
+        sampleLista.add(34);
+        sampleLista.add(12);
+        sampleLista.add(12);
+        sampleLista.add(3);
+        sampleLista.add(35);
+        sampleLista.add(10);
+        sampleLista.add(3);
+        sampleLista.add(4);
+        sampleLista.add(4);
+        sampleLista.add(11);
+        sampleLista.add(10);
+        System.out.println(ExBruno.exD(sampleLista));
     }
 }
